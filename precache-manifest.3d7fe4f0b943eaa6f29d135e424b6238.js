da033716dbd09e35beef451d1c6b6e074306bc19
syedahmed95 <syedahmed978@msn.com>
2019-09-13 17:55:12 +0500

    Updates

---
+++ precache-manifest.3d7fe4f0b943eaa6f29d135e424b6238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f71355380f133ebdf2136c2f6bd5a3b",
+    "url": "/E-Shop/index.html"
+  },
+  {
+    "revision": "8a4eea0e379d2687fbba",
+    "url": "/E-Shop/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "40b9e3a2cdc10591ed3e",
+    "url": "/E-Shop/static/css/main.78b2f535.chunk.css"
+  },
+  {
+    "revision": "8a4eea0e379d2687fbba",
+    "url": "/E-Shop/static/js/2.25d86b13.chunk.js"
+  },
+  {
+    "revision": "40b9e3a2cdc10591ed3e",
+    "url": "/E-Shop/static/js/main.8b9b298a.chunk.js"
+  },
+  {
+    "revision": "e43a7e71baab6f7c2f63",
+    "url": "/E-Shop/static/js/runtime~main.9c264d29.js"
+  }
+]);
